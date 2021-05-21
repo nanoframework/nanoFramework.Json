75c59f1bf2940f573c039a4e7fa723f883242c7c
Robin Jones <robin@networkfusion.co.uk>
2021-05-21 20:48:58 +0100

    Fix Double.NaN issue.

--- nanoFramework.Json/JsonConvert.cs
+++ nanoFramework.Json/JsonConvert.cs
@@ -654,9 +654,13 @@ namespace nanoFramework.Json
             }
             else if (token.TType == TokenType.Number)
             {
-                if (token.TValue.IndexOfAny(new char[] { 'e', 'E' }) != -1)
+                if (token.TValue.IndexOfAny(new char[] { 'd', 'D', 'e', 'E' }) != -1)
                 {
                     return new JsonValue(double.Parse(token.TValue));
+                }
+                else if (token.TValue.IndexOfAny(new char[] { '.', 'f', 'F' }) != -1)
+                {
+                    return new JsonValue(float.Parse(token.TValue));
                 }
                 else
                 {
