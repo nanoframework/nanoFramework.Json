931a24557a828b3e010767c6db916fd0fa800773
Robin Jones <robin@networkfusion.co.uk>
2021-05-21 17:44:58 +0100

    Update nugets. Revert using real literals

--- nanoFramework.Json.Test/JsonUnitTests.cs
+++ nanoFramework.Json.Test/JsonUnitTests.cs
@@ -188,14 +188,14 @@ namespace nanoFramework.Json.Test
                 byteArray = new[] { (byte)0x22, (byte)0x23, (byte)0x24, (byte)0x25, (byte)0x26 },
                 stringArray = new[] { "two", "four", "six", "eight" },
                 floatArray = new[] { 1.1f, 3.3f, 5.5f, 7.7f, 9.9f },
-                doubleArray = new[] { 1.1e, 3.3e, 5.5e, 7.7e, 9.9e },
+                doubleArray = new[] { 1.1, 3.3, 5.5, 7.7, 9.9 },
                 child1 = new JsonTestClassChild() { one = 1, two = 2, three = 3 },
                 Child = new JsonTestClassChild() { one = 100, two = 200, three = 300 },
                 nullObject = null,
                 nanFloat = float.NaN,
                 nanDouble = double.NaN,
                 aFloat = 1.2345f,
-                aDouble = 1.2345e,
+                aDouble = 1.2345,
                 aBoolean = true
             };
             var result = JsonConvert.SerializeObject(test);
