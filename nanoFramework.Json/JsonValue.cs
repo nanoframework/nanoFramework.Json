431855658b3061e586c4fe05d7f81e610ddcbab5
Laurent Ellerbach <laurelle@microsoft.com>
2021-11-11 16:49:30 +0100

    Fixing spaces vs tabs

--- nanoFramework.Json/JsonValue.cs
+++ nanoFramework.Json/JsonValue.cs
@@ -8,82 +8,82 @@ using System;
 
 namespace nanoFramework.Json
 {
-	internal class JsonValue : JsonToken
-	{
-		public JsonValue()
-		{
-		}
+    internal class JsonValue : JsonToken
+    {
+        public JsonValue()
+        {
+        }
 
-		public JsonValue(object value, bool isDateTime = false)
-		{
-			if (isDateTime)
-			{
-				DateTime dtValue = DateTimeExtensions.ConvertFromString((string)value);
-				
-				if (dtValue != DateTime.MaxValue)
-				{
-					Value = dtValue;
-				}
-			}
-			else
-			{
-				Value = value;
-			}
-		}
+        public JsonValue(object value, bool isDateTime = false)
+        {
+            if (isDateTime)
+            {
+                DateTime dtValue = DateTimeExtensions.ConvertFromString((string)value);
 
-		public object Value { get; set; }
-
-		public static JsonValue Serialize(Type type, object oValue)
-		{
-			if (type.Name == "Single" && float.IsNaN((float)oValue))
-			{
-			    	//Unfortunately JSON does not understand "float.NaN". This is the next best option!
-				return new JsonValue() { Value = null }; 
-			}
-			else if (type.Name == "Double" && double.IsNaN((double)oValue))
-			{
-				//Unfortunately JSON does not understand "float.NaN". This is the next best option!
-				return new JsonValue() { Value = null }; 
-			}
-
-			return new JsonValue() { Value = oValue };
-		}
-
-		public override string ToString()
-		{
-			EnterSerialization();
-
-			try
-			{
-				if (Value == null)
-				{
-					return "null";
-				}
-
-				var type = Value.GetType();
-
-				if (type == typeof(string) || type == typeof(char))
-				{
-					return "\"" + this.Value.ToString() + "\"";
-				}
-				else if (type == typeof(DateTime))
-				{
-					return "\"" + DateTimeExtensions.ToIso8601(((DateTime)Value)) + "\"";
-				}
-				else if(type == typeof(bool))
+                if (dtValue != DateTime.MaxValue)
                 {
-					// need to convert Boolean values to lower case 
-					return Value.ToString().ToLower();
+                    Value = dtValue;
                 }
-				else
-				{
-					return Value.ToString();
-				}
-			}
-			finally
-			{
-				ExitSerialization();
-			}
-		}
-	}
+            }
+            else
+            {
+                Value = value;
+            }
+        }
+
+        public object Value { get; set; }
+
+        public static JsonValue Serialize(Type type, object oValue)
+        {
+            if (type.Name == "Single" && float.IsNaN((float)oValue))
+            {
+                //Unfortunately JSON does not understand "float.NaN". This is the next best option!
+                return new JsonValue() { Value = null };
+            }
+            else if (type.Name == "Double" && double.IsNaN((double)oValue))
+            {
+                //Unfortunately JSON does not understand "float.NaN". This is the next best option!
+                return new JsonValue() { Value = null };
+            }
+
+            return new JsonValue() { Value = oValue };
+        }
+
+        public override string ToString()
+        {
+            EnterSerialization();
+
+            try
+            {
+                if (Value == null)
+                {
+                    return "null";
+                }
+
+                var type = Value.GetType();
+
+                if (type == typeof(string) || type == typeof(char))
+                {
+                    return "\"" + this.Value.ToString() + "\"";
+                }
+                else if (type == typeof(DateTime))
+                {
+                    return "\"" + DateTimeExtensions.ToIso8601(((DateTime)Value)) + "\"";
+                }
+                else if (type == typeof(bool))
+                {
+                    // need to convert Boolean values to lower case 
+                    return Value.ToString().ToLower();
+                }
+                else
+                {
+                    return Value.ToString();
+                }
+            }
+            finally
+            {
+                ExitSerialization();
+            }
+        }
+    }
 }
