1b7d2d5c45dcbe2d61b02eedc3695d13950c9851
Marcin Torba <torbamarcin@gmail.com>
2022-09-24 14:23:55 +0200

    Summary for IConverter

--- nanoFramework.Json/Converters/IConverter.cs
+++ nanoFramework.Json/Converters/IConverter.cs
@@ -3,11 +3,23 @@ using System.Text;
 
 namespace nanoFramework.Json.Converters
 {
-    //TODO: comments and tests
-    // Tests should: Check values and types
+    /// <summary>
+    /// Interface for all JSON converters.
+    /// </summary>
     public interface IConverter
     {
+        /// <summary>
+        /// Converts JSON string to object.
+        /// </summary>
+        /// <param name="value">JSON value to convert from.</param>
+        /// <returns>Object converted to type.</returns>
         object ToType(object value);
+
+        /// <summary>
+        /// Converts object into JSON string.
+        /// </summary>
+        /// <param name="value">Value to convert from.</param>
+        /// <returns>String with JSON value.</returns>
         string ToJson(object value);
     }
 }
