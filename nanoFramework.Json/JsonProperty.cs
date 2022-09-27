6a5c12c6c3007f48e4d069955898ede9972dc0a7
Marcin Torba <torbamarcin@gmail.com>
2022-09-27 22:09:27 +0200

    Clean up unused private/internal methods.

--- nanoFramework.Json/JsonProperty.cs
+++ nanoFramework.Json/JsonProperty.cs
@@ -22,40 +22,5 @@ namespace nanoFramework.Json
 
         public string Name { get; set; }
         public JsonToken Value { get; set; }
-
-        public override string ToString()
-        {
-            EnterSerialization();
-
-            StringBuilder sb = new(); //TODO: why move out of the try?
-
-            try
-            {
-                //Use minimalist JSON, pretty can be handled on the client!
-                sb.Append('"');
-                sb.Append(Name);
-                sb.Append("\":");
-
-                JsonToken token = Value;
-
-                if (token is JsonValue j
-                    && j.Value != null
-                    && j.Value.GetType().Name == "Boolean")
-                {
-                    // need to convert Boolean values to lower case 
-                    sb.Append(Value.ToString().ToLower());
-
-                    return sb.ToString();
-                }
-
-                sb.Append(Value.ToString());
-
-                return sb.ToString();
-            }
-            finally
-            {
-                ExitSerialization();
-            }
-        }
     }
 }
