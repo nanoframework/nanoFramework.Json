2192adf6a7208b4f0bbd51703127b15871b8f8b8
Marcin Torba <torbamarcin@gmail.com>
2022-09-28 16:07:56 +0200

    Clean up unused private/internal methods (#250)

--- nanoFramework.Json/JsonArray.cs
+++ nanoFramework.Json/JsonArray.cs
@@ -25,51 +25,5 @@ namespace nanoFramework.Json
         public int Length => Items.Length;
 
         public JsonToken[] Items { get; }
-
-        public override string ToString()
-        {
-            // set up a SerializationContext object and Lock it (via Monitor)
-            EnterSerialization();
-
-            try
-            {
-                StringBuilder sb = new();
-
-                sb.Append('[');
-
-                int prefaceLength = 0;
-                bool first = true;
-
-                foreach (var item in Items)
-                {
-                    if (!first)
-                    {
-                        if (sb.Length - prefaceLength > 72)
-                        {
-                            // Use minimalist JSON, pretty can be handled by the client!
-                            sb.Append(",");
-
-                            prefaceLength = sb.Length;
-                        }
-                        else
-                        {
-                            sb.Append(',');
-                        }
-                    }
-
-                    first = false;
-
-                    sb.Append(item);
-                }
-
-                sb.Append(']');
-
-                return sb.ToString();
-            }
-            finally
-            {
-                ExitSerialization();    // Unlocks the SerializationContext object
-            }
-        }
     }
 }
--- nanoFramework.Json/JsonObject.cs
+++ nanoFramework.Json/JsonObject.cs
@@ -16,79 +16,11 @@ namespace nanoFramework.Json
     {
         private readonly Hashtable _members = new();
 
-        public JsonProperty this[string name]
-        {
-            get { return (JsonProperty)_members[name.ToLower()]; }
-            set
-            {
-                if (name.ToLower() != value.Name.ToLower())
-                {
-                    throw new ArgumentException("index value must match property name");
-                }
-
-                _members.Add(value.Name.ToLower(), value);
-            }
-        }
-
-        public bool Contains(string name) => this._members.Contains(name.ToLower());
-
         public ICollection Members => _members.Values;
-
-        public static object JsonObjectAttribute { get; private set; }
 
         public void Add(string name, JsonToken value)
         {
             _members.Add(name.ToLower(), new JsonProperty(name, value));
-        }
-
-        //Use minimalist JSON, pretty can be handled on the client!
-        public override string ToString()
-        {
-            // set up a SerializationContext object and Lock it (via Monitor)
-            EnterSerialization();
-
-            try
-            {
-                StringBuilder sb = new();
-
-                sb.Append("{");
-
-                bool first = true;
-                Type type;
-
-                foreach (var key in _members.Keys)
-                {
-                    var member = _members[key];
-                    if (!first)
-                    {
-                        sb.Append(",");
-                    }
-
-                    first = false;
-
-                    type = member.GetType();
-                    if (type == typeof(JsonProperty))
-                    {
-                        sb.Append(((JsonProperty)member).ToString());
-                    }
-                    else if (type == typeof(JsonObject))
-                    {
-                        sb.Append($"\"{key}\":{(JsonObject)member}");
-                    }
-                    else if (type == typeof(JsonArray))
-                    {
-                        sb.Append(((JsonArray)member).ToString());
-                    }
-                }
-
-                sb.Append("}");
-
-                return sb.ToString();
-            }
-            finally
-            {
-                ExitSerialization();    // Unlocks the SerializationContext object
-            }
         }
     }
 }
