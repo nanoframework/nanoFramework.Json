1bcd781853234d621fd7353cdd49c997c34d7177
jazzman55 <jazzman5@poczta.onet.pl>
2022-10-26 09:26:09 +0200

    Revert "two-byte chars support only (inline)"
    This reverts commit 7544dc2968970fa4e0310f022177e9918befd5e1.

--- nanoFramework.Json/JsonConvert.cs
+++ nanoFramework.Json/JsonConvert.cs
@@ -1050,7 +1050,9 @@ namespace nanoFramework.Json
                 }
 
                 ch = (jsonBytes[jsonPos] & 0x80) == 0 ? (char)jsonBytes[jsonPos++]
-                    : Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 2) - 2, 2)[0];
+                    : (jsonBytes[jsonPos] & 0x20) == 0 ? Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 2) - 2, 2)[0]
+                    : (jsonBytes[jsonPos] & 0x10) == 0 ? Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 3) - 3, 3)[0]
+                    : Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 4) - 4, 4)[0];
 
                 // Handle json escapes
                 bool escaped = false;
@@ -1060,7 +1062,9 @@ namespace nanoFramework.Json
                 {
                     escaped = true;
                     ch = (jsonBytes[jsonPos] & 0x80) == 0 ? (char)jsonBytes[jsonPos++]
-                        : Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 2) - 2, 2)[0];
+                        : (jsonBytes[jsonPos] & 0x20) == 0 ? Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 2) - 2, 2)[0]
+                        : (jsonBytes[jsonPos] & 0x10) == 0 ? Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 3) - 3, 3)[0]
+                        : Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 4) - 4, 4)[0];
 
                     if (ch == (char)0xffff)
                     {
@@ -1271,7 +1275,9 @@ namespace nanoFramework.Json
         private static void Expect(char expected)
         {
             char ch = (jsonBytes[jsonPos] & 0x80) == 0 ? (char)jsonBytes[jsonPos++]
-                : Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 2) - 2, 2)[0];
+                : (jsonBytes[jsonPos] & 0x20) == 0 ? Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 2) - 2, 2)[0]
+                : (jsonBytes[jsonPos] & 0x10) == 0 ? Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 3) - 3, 3)[0]
+                : Encoding.UTF8.GetChars(jsonBytes, (jsonPos += 4) - 4, 4)[0];
 
             if (ch.ToLower() != expected)
             {
