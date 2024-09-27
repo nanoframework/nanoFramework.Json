13f8c2868e3c13b5e89dd21a23cc4b9b68f18839
Cory Charlton <CoryCharlton@users.noreply.github.com>
2024-09-26 20:03:40 -0700

    Adding type comparison benchmarks

---
+++ nanoFramework.Json.Benchmark/BenchmarkBase.cs
@@ -0,0 +1,22 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+
+namespace nanoFramework.Json.Benchmark
+{
+    public abstract class BenchmarkBase
+    {
+        protected virtual int IterationCount => 20;
+
+        public void RunInIteration(Action methodToRun)
+        {
+            for (var i = 0; i < IterationCount; i++)
+            {
+                methodToRun();
+            }
+        }
+    }
+}
